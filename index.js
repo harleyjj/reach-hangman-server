33f45e24e8ea62cf1ec9c38eaba6418e6c4f9872
harleyjj <harleyyesm@gmail.com>
2018-08-03 10:25:26 -0700

    slight formatting

--- index.js
+++ index.js
@@ -11,63 +11,63 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const app = express();
 
 let catData = [{imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-name: 'Fluffy',
-sex: 'Female',
-age: 2,
-breed: 'Bengal',
-story: 'Thrown on the street'},
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'},
 {imageURL:'https://www.telegraph.co.uk/content/dam/women/2018/01/30/TELEMMGLPICT000152840203_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwViJj1eTvcjzL4JkNP_PJEs.jpeg', 
-imageDescription: 'Black cat with white tummey.',
-name: 'Namey',
-sex: 'Female',
-age: 3,
-breed: 'Cat',
-story: 'Born on the 4th of July'},
+  imageDescription: 'Black cat with white tummey.',
+  name: 'Namey',
+  sex: 'Female',
+  age: 3,
+  breed: 'Cat',
+  story: 'Born on the 4th of July'},
 {imageURL:'http://nostalgiacentral.com/wp-content/uploads/2014/06/pinkpanther_151.jpg', 
-imageDescription: 'Pink cat with an attitude.',
-name: 'Pinky',
-sex: 'Male',
-age: 75,
-breed: 'Fictional',
-story: 'Thown out for live action version'},
+  imageDescription: 'Pink cat with an attitude.',
+  name: 'Pinky',
+  sex: 'Male',
+  age: 75,
+  breed: 'Fictional',
+  story: 'Thown out for live action version'},
 {imageURL:'https://www.garfieldgo.com/images/GarfieldCurious.png', 
-imageDescription: 'Orange cat with an attitude.',
-name: 'Garfield',
-sex: 'Male',
-age: 65,
-breed: 'Lazy',
-story: 'He hates mondays but he might like you?'}
-]
+  imageDescription: 'Orange cat with an attitude.',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 65,
+  breed: 'Lazy',
+  story: 'He hates mondays but he might like you?'}
+];
 
 let dogData = [{imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-name: 'Zeus',
-sex: 'Male',
-age: 3,
-breed: 'Golden Retriever',
-story: 'Owner Passed away'},
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'},
 {imageURL: 'https://vignette.wikia.nocookie.net/thefrasier/images/b/b2/Frasierwoody41eddie-48ajwekt2.jpg',
-imageDescription: 'He loves that old chair.',
-name: 'Eddie',
-sex: 'Male',
-age: 30,
-breed: 'Russell Terrier',
-story: 'Show ended after 9 seasons'},
+  imageDescription: 'He loves that old chair.',
+  name: 'Eddie',
+  sex: 'Male',
+  age: 30,
+  breed: 'Russell Terrier',
+  story: 'Show ended after 9 seasons'},
 {imageURL: 'https://uproxx.files.wordpress.com/2015/02/triumph.jpeg',
-imageDescription: 'Triumph smoking a cigar.',
-name: 'Triumph',
-sex: 'Male',
-age: 30,
-breed: 'Puppet',
-story: 'Living in your home sounds great.... FOR ME TO POOP ON!'},
+  imageDescription: 'Triumph smoking a cigar.',
+  name: 'Triumph',
+  sex: 'Male',
+  age: 30,
+  breed: 'Puppet',
+  story: 'Living in your home sounds great.... FOR ME TO POOP ON!'},
 {imageURL: 'https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Scooby/characterArt-scooby-SD.png',
-imageDescription: 'Scooby Snacks?',
-name: 'Scooby-Do',
-sex: 'Male',
-age: 75,
-breed: 'Great Dane',
-story: 'Only survivor of mystery machine accident (rut roh)'}]
+  imageDescription: 'Scooby Snacks?',
+  name: 'Scooby-Do',
+  sex: 'Male',
+  age: 75,
+  breed: 'Great Dane',
+  story: 'Only survivor of mystery machine accident (rut roh)'}];
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -83,28 +83,28 @@ app.use(
 
 app.get('/api/cat', (req, res) => {
   if(catData.length === 0){
-    res.json({message: "No more cats to adopt!"})
+    res.json({message: 'No more cats to adopt!'});
   } else {
-    res.json(catData[0])
+    res.json(catData[0]);
   }
   
-})
+});
 
 app.get('/api/dog', (req, res) => {
   if(dogData.length === 0){
-    res.json({message: "No more dogs to adopt!"})
+    res.json({message: 'No more dogs to adopt!'});
   } else {
-    res.json(dogData[0])
+    res.json(dogData[0]);
   }
-})
+});
 
 app.delete('/api/cat', (req, res) => {
-  res.json(catData.shift())
-})
+  res.json(catData.shift());
+});
 
 app.delete('/api/dog', (req, res) => {
-  res.json(dogData.shift())
-})
+  res.json(dogData.shift());
+});
 
 function runServer(port = PORT) {
   const server = app
